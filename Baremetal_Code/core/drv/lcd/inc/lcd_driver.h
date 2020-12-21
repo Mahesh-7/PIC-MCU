ddc0265c1abb7b65c73d81c38f049839bdf90b95
MAHESH-K <60285145+Mahesh-7@users.noreply.github.com>
2020-12-21 19:46:32 +0530

    Update lcd_driver.h

--- Baremetal_Code/core/drv/lcd/inc/lcd_driver.h
+++ Baremetal_Code/core/drv/lcd/inc/lcd_driver.h
@@ -1,3 +1,7 @@
+#ifndef lcd
+#define lcd
+
+#define _XTAL_FREQ 4000000
 
 void lcd_cmd(unsigned char cmd);
 
@@ -6,3 +10,6 @@ void lcd_char(unsigned char data);
 void lcd_string(const char * ptr);
 
 void lcd_init();
+
+#endif
+
